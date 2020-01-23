48d637e4442ffd0a61730d57df3aa79b475a33ee
shiuai <1219363257@qq.com>
2020-01-23 20:27:34 +0800

    增加eslint校验发布0.0.7版

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,32 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-unused-vars": [2, { 
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      "args": "none" 
+    }],
+    // 关闭语句强制分号结尾
+    "semi": [0],
+    //空行最多不能超过100行
+    "no-multiple-empty-lines": [0, {"max": 100}],
+    //关闭禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": [0],
+    "no-trailing-spaces": 1,//一行结束后面不要有空格
+    "no-unused-vars": [2, {"vars": "all", "args": "after-used"}],//不能有声明后未被使用的变量或参数
+    "space-before-blocks": [0, "always"],//不以新行开始的块{前面要不要有空格
+    "space-before-function-paren": [0, "always"],//函数定义时括号前面要不要有空格
+    "space-infix-ops": 0,//中缀操作符周围要不要有空格
+    "keyword-spacing": 0,//return throw case后面要不要加空格
+    "indent": [2, 4],//缩进风格
+    "key-spacing": [0, { "beforeColon": false, "afterColon": true }],//对象字面量中冒号的前后空格
+    "comma-spacing": 0,//逗号前后的空格
+    "comma-dangle": [2, "never"],//对象字面量项尾不能有逗号
+    "semi-spacing": [0, {"before": false, "after": true}],//分号前后空格
+    "eqeqeq": 0,//必须使用全等
+    "eol-last": 0,//文件以单一的换行符结束
+    "spaced-comment": 0,//注释风格要不要有空格什么的
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+// /* eslint-disable no-new */
+// new Vue({
+//     el: '#app',
+//     router,
+//     components: { App },
+//     template: '<App/>'
+// })
+
+import Calendar from './components/Calendar/Calendar.vue';
+import Input from './components/Input/Input.vue';
+
+// install 是 用来全部安装的关键
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(install)
+}
+
+function install (Vue) {
+    Vue.component(Calendar.name, Calendar)
+    Vue.component(Input.name, Input)
+}
+
+export default {
+    install,
+    Calendar,
+    Input
+}
